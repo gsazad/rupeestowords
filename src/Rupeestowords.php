81aede1f514b85c3f0e4323622bb7ff382bd806f
Gurjeet Singh <singh@gurjeet.co.in>
2024-07-14 16:43:35 +0530

    fix forty

--- src/Rupeestowords.php
+++ src/Rupeestowords.php
@@ -316,7 +316,7 @@ class Rupeestowords
         '6' => 'six','7' => 'seven','8' => 'eight','9' => 'nine','10' => 'ten',
         '11' => 'eleven','12' => 'twelve','13' => 'thirteen','14' => 'fouteen','15' => 'fifteen',
         '16' => 'sixteen','17' => 'seventeen','18' => 'eighteen','19' => 'nineteen','20' => 'twenty',
-        '30' => 'thirty','40' => 'fourty','50' => 'fifty','60' => 'sixty','70' => 'seventy',
+        '30' => 'thirty','40' => 'forty','50' => 'fifty','60' => 'sixty','70' => 'seventy',
         '80' => 'eighty','90' => 'ninty');
         
         //First find the length of the number
